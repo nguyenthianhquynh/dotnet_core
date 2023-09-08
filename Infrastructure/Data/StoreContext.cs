505f73fa63779aec952c8ad683032b00668add69
quynh-nta <quynh-nta@blockhive.vn>
2023-09-08 18:13:26 +0700

    create table product & seed data product

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -16,6 +16,9 @@ namespace Infrastructure.Data
         public DbSet<Role> Roles {get; set;}
         public DbSet<Permission> Permissions {get; set;}
         public DbSet<RolePermission> RolePermissions {get; set;}
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
